df3749756d8ccdcdc4b11be0f18a76d861d106b7
Philip Turner <philipturner.AR@gmail.com>
2022-04-18 19:56:02 -0400

    Add regression tests

--- Package.swift
+++ Package.swift
@@ -6,18 +6,14 @@ import PackageDescription
 let package = Package(
   name: "_Differentiation",
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "_Differentiation",
       targets: ["Differentiation"]),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
+
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "_Differentiation",
       dependencies: []),
@@ -25,10 +21,64 @@ let package = Package(
       name: "Differentiation",
       dependencies: ["_Differentiation"],
       swiftSettings: [.unsafeFlags(["-parse-stdlib"])]),
+
     // TODO: Make a separate module with each test for the specific compiler flags it needs
-    // TODO: Allow more tests that make the compiler unhappy
+    // TODO: Include tests that require compilation of multiple modules
     .testTarget(
       name: "DifferentiationTests",
-      dependencies: ["Differentiation"]),
+      dependencies: ["Differentiation"],
+      exclude: [
+        // Crashes on the Xcode 13.3.1 toolchain
+        "compiler_crashers_fixed/sr13933-vjpcloner-apply-multiple-consuming-users.swift",
+      ],
+      sources: [
+        "DifferentiationTests.swift",
+        "compiler_crashers_fixed/rdar71191415-nested-differentiation-of-extension-method-optimized.swift",
+        "compiler_crashers_fixed/rdar71319547-generated-decls-shall-not-be-resilient.swift",
+        "compiler_crashers_fixed/rdar74087329-debug-scope-trampoline-blocks.swift",
+        "compiler_crashers_fixed/rdar84716758-differentiable-protocol-witness-missing-requirements.swift",
+        "compiler_crashers_fixed/sr12548-siloptimizer-rewrite-partial-apply-convention-method.swift",
+        "compiler_crashers_fixed/sr12559-derivative-attr-type-checking-parse-stdlib.swift",
+        "compiler_crashers_fixed/sr12641-silgen-immutable-address-use-verification-failure.swift",
+        "compiler_crashers_fixed/sr12642-differentiable-derivation-redeclared-property.swift",
+        "compiler_crashers_fixed/sr12650-noderivative-parameter-type-mangling.swift",
+        "compiler_crashers_fixed/sr12656-differentiation-opaque-result-type.swift",
+        "compiler_crashers_fixed/sr12744-unhandled-pullback-indirect-result.swift",
+        "compiler_crashers_fixed/sr12886-sr12887-vjp-emitter-definite-initialization.swift",
+        "compiler_crashers_fixed/sr13305-noderivative-inout-parameter.swift",
+        "compiler_crashers_fixed/sr13411-tangent-value-category-mismatch.swift",
+        "compiler_crashers_fixed/sr13865-library-evolution-mode-crasher-property-differentiation.swift",
+        "compiler_crashers_fixed/sr14240-symbol-in-ir-file-not-tbd-file.swift",
+        "compiler_crashers_fixed/sr14290-missing-debug-scopes-in-pullback-trampoline.swift",
+        "compiler_crashers_fixed/sr14625-over-consume-in-subset-parameters-thunk.swift",
+        "compiler_crashers_fixed/sr15205-diff-capture.swift",
+        "compiler_crashers_fixed/sr15566-vardecl-adjoint-values.swift",
+        // TODO: Reproduce the correct conditions for SR-15849 to trigger a crash
+        "compiler_crashers_fixed/sr15849-mutating-functions-with-control-flow.swift",
+        "compiler_crashers_fixed/sr15891-inout-adjoint-param-idx.swift",
+        "compiler_crashers_fixed/tf1160-derivative-usable-from-inline-mismatch.swift",
+        "compiler_crashers_fixed/tf1180-silgen-vtable-method-does-not-appear.swift",
+        "compiler_crashers_fixed/tf1232-autodiff-generated-declaration-mangling.swift",
+        "compiler_crashers_fixed/tf1315-pullback-subset-parameter-thunk-generation.swift",
+      ])
   ]
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
