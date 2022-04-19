b91c325265341baa7092d4a9c03c3e613e3d8349
Philip Turner <philipturner.AR@gmail.com>
2022-04-18 20:00:01 -0400

    Fix up script

--- build_gyb.sh
+++ build_gyb.sh
@@ -8,15 +8,11 @@ for file in $(ls "$gyb_sources")
 do
   new_length=$(expr ${#file} - 4)
   new_name=${file:0:new_length}
+
   new_path="${swift_sources}/${new_name}"
-  
   old_path="${gyb_sources}/${file}"
   line_directive_path="../gyb-sources/${file}"
-  
-  echo $new_path
-  echo $old_path
+
   python3 gyb.py "$old_path" -o "$new_path" --line-directive \
     "#sourceLocation(file: \"${line_directive_path}\", line: %(line)d)"
-  
-  echo $new_name
 done
